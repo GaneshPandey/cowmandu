00ae055a54a50544c6b5d3beef94f1ccd6f91838
GaneshPandey <footballcrazey@gmail.com>
2019-10-15 12:55:24 +0545

    create users app

---
+++ app/users/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    CUSTOMER = 'customer'
+    MANAGER = 'manager'
+
+    TYPE_CHOICES = (
+        (CUSTOMER, 'Customer'),
+        (MANAGER, 'Manager')
+    )
+
+    type = models.CharField(
+        choices=TYPE_CHOICES,
+        default=CUSTOMER,
+        max_length=10
+    )
+
+    is_subscriber = models.BooleanField(default=False)
+
+    @property
+    def is_manager(self):
+        return self.type == self.MANAGER
+
+    @property
+    def is_customer(self):
+        return self.type == self.CUSTOMER
+
+    def __str__(self):
+        return self.get_username()
