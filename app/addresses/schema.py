ffc9b410244fe268cd96b1e98adf2ad2e5d1e048
GaneshPandey <footballcrazey@gmail.com>
2019-10-17 16:00:34 +0545

    add address mutation

--- app/addresses/schema.py
+++ app/addresses/schema.py
@@ -9,6 +9,47 @@ class AddressType(DjangoObjectType):
         model = Address
 
 
+class CreateAddress(graphene.Mutation):
+    address = graphene.Field(AddressType)
+
+    class Arguments:
+        nearby_location = graphene.String()
+        address_line_2 = graphene.String()
+        address_line_1 = graphene.String(required=True)
+        city = graphene.String(required=True)
+        latitude = graphene.String()
+        longitude = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        nearby_location = kwargs.get('nearby_location')
+        address_line_2 = kwargs.get('address_line_2')
+        address_line_1 = kwargs.get('address_line_1')
+        city = kwargs.get('city')
+        latitude = kwargs.get('latitude')
+        longitude = kwargs.get('longitude')
+
+        address = Address(
+            nearby_location=nearby_location,
+            address_line_2=address_line_2,
+            address_line_1=address_line_1,
+            city=city,
+            latitude=latitude,
+            longitude=longitude
+        )
+        address.save()
+        return CreateAddress(address=address)
+
+
+class UpdateAddress(graphene.Mutation):
+    address = graphene.Field(AddressType)
+
+    class Arguments:
+        nearby_location = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        pass
+
+
 class Query(graphene.ObjectType):
     addresses = graphene.List(AddressType)
 
@@ -16,4 +57,6 @@ class Query(graphene.ObjectType):
         return Address.objects.all()
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(graphene.ObjectType):
+    create_address = CreateAddress.Field()
+    update_address = UpdateAddress.Field()
